2b7e680623ff31f7f1f17d9d7f26fd3863113684
Phuong Cao <pcao3@illinois.edu>
2017-11-17 07:27:31 -0600

    update server code

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 SVAuth Python Platform
-Time-stamp: <2017-11-17 07:22:19 phuong>
+Time-stamp: <2017-11-17 07:27:17 phuong>
 """
 
 import os
@@ -9,12 +9,53 @@ import requests
 import json
 
 from flask import Flask, request, session, redirect, render_template, make_response
-from .utils import init_token, validate_user, populate_session, request_userprofile, init_session
 
 CHECK_AUTHCODE_URL = "https://authjs.westus.cloudapp.azure.com:3020/CheckAuthCode?authcode={}"
 RELYING_PARTY = "https://svauth-python-adapter.herokuapp.com?py"
 START_URL = "https://authjs.westus.cloudapp.azure.com:3020/login/Facebook?token={}&concdst={}"
 AUTHORIZED_USERS = ["Phuong Cao"]
+
+
+def init_session():
+    """
+    Init an empty session
+    """
+    if "UserID" not in session:
+        session["UserID"] = ""
+
+
+def init_token():
+    """
+    Init a token key used to validate user profile returned from the public agent
+    """
+    import hashlib
+    MAX_TOKEN_LENGTH = 38
+    sid_sha256 = hashlib.sha256(
+        request.cookies.get('session').encode('utf-8')).hexdigest()
+    token = sid_sha256[:MAX_TOKEN_LENGTH]
+    session["token"] = sid_sha256[:MAX_TOKEN_LENGTH]
+    return token
+
+
+def validate_user(resp):
+    if session["FullName"] not in AUTHORIZED_USERS:
+        raise "unauthorized"
+
+    if ('token' not in resp) or \
+       (session["token"] != resp['conckey']):
+        raise "invalid token"
+
+
+def populate_session(resp):
+    fields = ["UserID", "FullName", "Email", "Authority"]
+    for field in fields:
+        session[field] = resp['userProfile'][field]
+
+
+def request_userprofile(authcode):
+    return json.loads(
+        requests.get(CHECK_AUTHCODE_URL.format(authcode), verify=False).text)
+
 
 app = Flask(__name__)
 
