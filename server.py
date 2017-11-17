e7c5c2157716e6a9fd22ba760b924159ece690d2
Phuong Cao <pcao3@illinois.edu>
2017-11-16 21:49:45 -0600

    update

--- server.py
+++ server.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 """
 SVAuth Python Platform
-Time-stamp: <2017-04-28 10:57:45 phuong>
+Time-stamp: <2017-11-16 21:42:32 phuong>
 """
 
 import os
+import requests
+import json
+
 import flask
 from flask import Flask, request, session, redirect, render_template, flash, abort, make_response, session
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
-max_conckey = 38
-IDP = "Facebook"
+MAX_CONCKEY_LENGTH = 38
+IDENTITY_PROVIDER = "Facebook"
 app = Flask(__name__)
 
 
@@ -21,8 +24,6 @@ def index():
     """
     Show an index page with social login buttons
     """
-    # if request.remote_addr != "127.0.0.1":
-    #     abort(403)
     if "UserID" in request.form and len(request.form["UserID"]) == 0:
         session.clear()
     resp = make_response(render_template("index.html"))
@@ -34,22 +35,6 @@ def index():
     return resp
 
 
-# from stackoverflow
-def decrypt(key, iv, ciphertext):
-    """
-    Decrypt a cipher text using AES-256-CBC
-    """
-    # Construct a Cipher object, with the key, iv, and additionally the
-    # GCM tag used for authenticating the message.
-    decryptor = Cipher(
-        algorithms.AES(key), modes.CBC(iv),
-        backend=default_backend()).decryptor()
-
-    # Decryption gets us the authenticated plaintext.
-    # If the tag does not match an InvalidTag exception will be raised.
-    return decryptor.update(ciphertext) + decryptor.finalize()
-
-
 @app.route('/SVAuth/adapters/py/RemoteCreateNewSession.py', methods=['GET'])
 def remote_create_new_session():
     """
@@ -57,23 +42,23 @@ def remote_create_new_session():
     Decode the encrypted user profile
     Set user profile to current session
     """
-    encryptedUserProfile = request.args.get("encryptedUserProfile")
-    key = session["key"]
-    key = bytes(key, 'utf-8')
-    key = key[:32]
-    iv = key[:16]
-    encryptedUserProfile = bytes(bytearray.fromhex(encryptedUserProfile))
-    res = decrypt(key, iv, encryptedUserProfile)
-    # remove padding
-    res = res.decode('utf-8')
-    end = res.rfind('}')
-    res = res[:end + 1]
-    # decode json
-    res = json.loads(res)
-    fields = ["UserID", "FullName", "Email", "Authority"]
-    for field in fields:
-        session[field] = res[field]
-    return redirect("/")
+    try:
+        url = '{}://{}:{}/CheckAuthCode?authcode={}'.format(
+            config['AgentSettings']['scheme'],
+            config['AgentSettings']['agentHostname'],
+            config['AgentSettings']['port'], request.args.get("authcode"))
+        req = requests.get(url, verify=False)
+        req_json = json.loads(req.text)
+        user_profile = req_json['userProfile']
+        if ('conckey' not in req_json) or \
+        (session["key"] != req_json['conckey']):
+            raise "invalid conckey"
+        fields = ["UserID", "FullName", "Email", "Authority"]
+        for field in fields:
+            session[field] = user_profile[field]
+        return redirect("/")
+    except:
+        return "exception"
 
 
 @app.route('/start', methods=['GET'])
@@ -82,18 +67,17 @@ def start():
     Start the login flow by contacting the remote svauth agent
     """
     import hashlib
-    max_conckey = 38
     sid_sha256 = hashlib.sha256(
         request.cookies.get('session').encode('utf-8')).hexdigest()
-    conckey = sid_sha256[:max_conckey]
+    conckey = sid_sha256[:MAX_CONCKEY_LENGTH]
     url = '{}://{}:{}/login/{}?conckey={}&concdst={}://{}?{}'.format(
         config['AgentSettings']['scheme'],
         config['AgentSettings']['agentHostname'],
-        config['AgentSettings']['port'], IDP, conckey,
+        config['AgentSettings']['port'], IDENTITY_PROVIDER, conckey,
         config['WebAppSettings']['scheme'],
         config['WebAppSettings']['hostname'],
         config['WebAppSettings']['platform']['name'])
-    session["key"] = sid_sha256[:max_conckey]
+    session["key"] = sid_sha256[:MAX_CONCKEY_LENGTH]
     return redirect(url)
 
 
@@ -104,16 +88,10 @@ def logout():
 
 
 if __name__ == '__main__':
-    import json
     global config
     app.debug = True
     app.secret_key = os.urandom(24)
-    config_file = "adapter_config.json"
-    if ("HEROKU" in os.environ):
-        config_file = "config/" + config_file
-    else:
-        config_file = "../adapter_config/" + config_file
-        # config_file = "config/" + config_file
+    config_file = "config/adapter_config.json"
     # read adapter config
     with open(config_file, encoding='utf-8') as data_file:
         config = json.loads(data_file.read())
